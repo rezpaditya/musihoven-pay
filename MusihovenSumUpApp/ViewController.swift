6a87ae2bcfdc6cfdd8176a3c012fa3d21f6fc5e2
rezpa.snk <rezpa.snk@gmail.com>
2024-05-11 19:47:03 +0200

    Initial Commit

---
+++ MusihovenSumUpApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusihovenSumUpApp
+//
+//  Created by Respa Aditya on 11/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MusihovenSumUpAppUITests/MusihovenSumUpAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MusihovenSumUpAppUITestsLaunchTests.swift
+//  MusihovenSumUpAppUITests
+//
+//  Created by Respa Aditya on 11/05/2024.
+//
+
+import XCTest
+
+final class MusihovenSumUpAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
